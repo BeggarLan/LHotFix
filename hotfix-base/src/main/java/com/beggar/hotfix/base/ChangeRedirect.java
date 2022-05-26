b0cbdd270ef54a1bba03b2135adde9994ce1fd80
BeggarLan <1348270542@qq.com>
2022-05-26 22:39:14 +0800

    完成代理类的isSupport方法

--- hotfix-base/src/main/java/com/beggar/hotfix/base/ChangeRedirect.java
+++ hotfix-base/src/main/java/com/beggar/hotfix/base/ChangeRedirect.java
@@ -1,5 +1,8 @@
 package com.beggar.hotfix.base;
 
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+
 /**
  * author: BeggarLan
  * created on: 2022/5/6 22:30
@@ -7,24 +10,24 @@ package com.beggar.hotfix.base;
  */
 public interface ChangeRedirect {
 
-    String sClassName = "com.beggar.hotfix.base.ChangeRedirect";
-    String sObjectName = "changeRedirect";
+  String sClassName = "com.beggar.hotfix.base.ChangeRedirect";
+  String sObjectName = "changeRedirect";
 
 
-    /**
-     * 是否可以patch
-     *
-     * @param methodName
-     * @param params
-     */
-    boolean isSupport(String methodName, Object[] params);
+  /**
+   * 是否可以patch
+   *
+   * @param patchMethodName 格式为isStatic:methodNumber
+   * @param params          最后一项为所属类对象(若是静态方法则没有类对象)。null代表函数无参数
+   */
+  boolean isSupport(@NotNull String patchMethodName, @Nullable Object[] params);
 
-    /**
-     * patch
-     *
-     * @param methodName
-     * @param params
-     */
-    Object patch(String methodName, Object[] params);
+  /**
+   * patch
+   *
+   * @param methodName
+   * @param params
+   */
+  Object patch(String methodName, Object[] params);
 
 }
--- patch/src/main/java/com/beggar/hotfix/patch/PatchProxy.java
+++ patch/src/main/java/com/beggar/hotfix/patch/PatchProxy.java
@@ -12,42 +12,78 @@ import com.beggar.hotfix.base.ChangeRedirect;
  */
 public class PatchProxy {
 
-    /**
-     * 方法是否支持修复
-     *
-     * @param params           参数
-     * @param FixClassThis     所在类对象，如果方法是static的话，该值为null
-     * @param changeRedirect   具体的修复类
-     * @param isStatic         是否是静态
-     * @param methodNumber     方法的索引
-     * @param paramsClassTypes 参数的类型
-     * @param returnType       函数返回值的类型
-     * @return {@code true} 支持修复
-     */
-    public static boolean isSupport(
-            @NonNull Object[] params,
-            @Nullable Object FixClassThis,
-            @Nullable ChangeRedirect changeRedirect,
-            boolean isStatic,
-            int methodNumber,
-            @NonNull Class<?>[] paramsClassTypes,
-            @NonNull Class<?> returnType) {
-         if(changeRedirect == null) {
-             return false;
-         }
-         
-        return false;
+  /**
+   * 方法是否支持修复
+   *
+   * @param params           参数
+   * @param FixClassThis     所在类对象，如果方法是static的话，该值为null
+   * @param changeRedirect   具体的修复类
+   * @param isStatic         是否是静态
+   * @param methodNumber     方法的索引
+   * @param paramsClassTypes 参数的类型
+   * @param returnType       函数返回值的类型
+   * @return {@code true} 支持修复
+   */
+  public static boolean isSupport(
+      @NonNull Object[] params,
+      @Nullable Object FixClassThis,
+      @Nullable ChangeRedirect changeRedirect,
+      boolean isStatic,
+      int methodNumber,
+      @NonNull Class<?>[] paramsClassTypes,
+      @NonNull Class<?> returnType) {
+    if (changeRedirect == null) {
+      return false;
     }
+    String proxyMethodStr = getProxyMethodStr(isStatic, methodNumber);
+    Object[] objects = getObjects(params, FixClassThis, isStatic);
+    return changeRedirect.isSupport(proxyMethodStr, objects);
+  }
 
-    public static void accessDispatchVoid(
-            @NonNull Object[] params,
-            @Nullable Object FixClassThis,
-            @NonNull ChangeRedirect changeRedirect,
-            boolean isStatic,
-            int methodNumber,
-            @NonNull Class<?>[] paramsClassTypes,
-            @NonNull Class<?> returnType) {
+  public static void accessDispatchVoid(
+      @NonNull Object[] params,
+      @Nullable Object FixClassThis,
+      @NonNull ChangeRedirect changeRedirect,
+      boolean isStatic,
+      int methodNumber,
+      @NonNull Class<?>[] paramsClassTypes,
+      @NonNull Class<?> returnType) {
 
+  }
+
+  /**
+   * 获取将要访问的方法标示
+   *
+   * @return isStatic:methodNumber
+   */
+  private static String getProxyMethodStr(boolean isStatic, int methodNumber) {
+    return isStatic + ":" + methodNumber;
+  }
+
+  /**
+   * @return 数组的前几个是函数的参数，最后一个是所属类对象；若函数参数为空，那么返回null
+   */
+  @Nullable
+  private static Object[] getObjects(Object[] params, Object fixClassThis, boolean isStatic) {
+    if (params == null) {
+      return null;
     }
+    Object[] objects;
+    int argLength = params.length;
+    // 静态方法没有类对象
+    if (isStatic) {
+      objects = new Object[argLength];
+    } else {
+      objects = new Object[argLength + 1];
+    }
+    int x = 0;
+    for (; x < argLength; x++) {
+      objects[x] = params[x];
+    }
+    if (!(isStatic)) {
+      objects[x] = fixClassThis;
+    }
+    return objects;
+  }
 
 }
