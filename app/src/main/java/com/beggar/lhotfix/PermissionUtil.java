bebace5d04dea2cbe863bc3ccbf84d0720807605
BeggarLan <1348270542@qq.com>
2022-05-26 23:21:52 +0800

    测试类

---
+++ app/src/main/java/com/beggar/lhotfix/PermissionUtil.java
@@ -0,0 +1,36 @@
+package com.beggar.lhotfix;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import androidx.annotation.NonNull;
+
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import io.reactivex.functions.Consumer;
+
+/**
+ * author: BeggarLan
+ * description: 权限工具类
+ */
+public class PermissionUtil {
+
+  /**
+   * 获取权限
+   *
+   * @param onNext      权限获取结果的回掉
+   * @param onError     权限获取错误的回掉
+   * @param permissions 要申请的权限
+   */
+  @SuppressLint("CheckResult")
+  public static void requestPermission(
+      @NonNull Activity activity,
+      Consumer<Boolean> onNext,
+      Consumer<? super Throwable> onError,
+      final String... permissions) {
+    RxPermissions rxPermissions = new RxPermissions(activity);
+    rxPermissions
+        .request(permissions)
+        .subscribe(onNext, onError);
+  }
+
+}
--- app/src/main/java/com/beggar/lhotfix/demo/MainActivity.java
+++ app/src/main/java/com/beggar/lhotfix/demo/MainActivity.java
@@ -1,17 +1,44 @@
 package com.beggar.lhotfix.demo;
 
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
+import com.beggar.lhotfix.PermissionUtil;
 import com.beggar.lhotfix.R;
 
 public class
 MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  private TextView mPatchButton;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    initView();
+  }
+
+  private void initView() {
+    mPatchButton = findViewById(R.id.patch_btn);
+    mPatchButton.setOnClickListener(view -> {
+      // 需要申请读写sd卡权限
+      PermissionUtil.requestPermission(
+          MainActivity.this,
+          isSuccess -> {
+            if (isSuccess) {
+              // 开始热修复
+              startHotfix();
+            }
+          },
+          throwable -> {},
+          Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE);
+    });
+  }
+
+  private void startHotfix() {
+
+  }
+
 }
