e4b7af915c01b41263da2662376208ce6cc92404
BeggarLan <1348270542@qq.com>
2022-06-10 23:05:47 +0800

    描述原类和补丁类

---
+++ patch/src/main/java/com/beggar/hotfix/patch/PatchClassInfo.java
@@ -0,0 +1,33 @@
+package com.beggar.hotfix.patch;
+
+import androidx.annotation.NonNull;
+
+/**
+ * author: lanweihua
+ * created on: 2022/6/10 10:59 下午
+ * description: 被修补的类和补丁类信息
+ */
+public class PatchClassInfo {
+
+  // 原类
+  @NonNull
+  private final String mSourceClassName;
+  // 补丁类
+  @NonNull
+  private final String mPatchClassName;
+
+  public PatchClassInfo(@NonNull String sourceClassName, @NonNull String patchClassName) {
+    mSourceClassName = sourceClassName;
+    mPatchClassName = patchClassName;
+  }
+
+  @NonNull
+  public String getSourceClassName() {
+    return mSourceClassName;
+  }
+
+  @NonNull
+  public String getPatchClassName() {
+    return mPatchClassName;
+  }
+}
---
+++ patch/src/main/java/com/beggar/hotfix/patch/PatchClassInfoProvider.java
@@ -0,0 +1,14 @@
+package com.beggar.hotfix.patch;
+
+import java.util.List;
+
+/**
+ * author: lanweihua
+ * created on: 2022/6/10 10:58 下午
+ * description: 用于获取[原类]和[补丁]类信息
+ */
+public interface PatchClassInfoProvider {
+
+  List<PatchClassInfo> getPatchClassInfoList();
+
+}
