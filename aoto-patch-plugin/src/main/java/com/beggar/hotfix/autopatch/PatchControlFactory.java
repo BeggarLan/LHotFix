1132281b30f9ab24be75f53ea8f14fd68192adc7
BeggarLan <1348270542@qq.com>
2022-07-21 12:43:36 +0800

    增加替换原类对象的方法

--- aoto-patch-plugin/src/main/java/com/beggar/hotfix/autopatch/PatchControlFactory.java
+++ aoto-patch-plugin/src/main/java/com/beggar/hotfix/autopatch/PatchControlFactory.java
@@ -3,8 +3,10 @@ package com.beggar.hotfix.autopatch;
 import com.android.annotations.NonNull;
 import com.beggar.hotfix.base.PatchTemplate;
 
+import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
+import javassist.CtMethod;
 import javassist.NotFoundException;
 
 /**
@@ -20,12 +22,32 @@ public class PatchControlFactory {
    */
   public static CtClass createPatchControlClass(
       @NonNull ClassPool classPool, @NonNull CtClass sourceClass, @NonNull CtClass patchClass)
-      throws NotFoundException {
+      throws NotFoundException, CannotCompileException {
     // 控制类名字
     String patchControlClassName =
         NameManager.getInstance().getPatchControlClassName(sourceClass.getSimpleName());
     CtClass patchControlCtClass =
         classPool.getAndRename(PatchTemplate.class.getName(), patchControlClassName);
+
+    // 添加获取参数真正类型的方法：方法是把原类对象替换成补丁类对象
+//    public Object getRealParameter(Object parameter) {
+//      if(parameter instanceOf SourceClassName) {
+//        return new PatchClass(parameter);
+//      }
+//      return parameter;
+//    }
+    StringBuilder getRealParameterMethodBuilder = new StringBuilder();
+    getRealParameterMethodBuilder
+        .append("public Object getRealParameter(Object parameter) {")
+        .append("if(parameter instanceOf " + sourceClass.getName() + ") {")
+        .append("return new " + patchClass.getName() + "(parameter);")
+        .append("}")
+        .append("return parameter")
+        .append("}");
+    patchControlCtClass
+        .addMethod(CtMethod.make(getRealParameterMethodBuilder.toString(), patchControlCtClass));
+
+
 
     return patchControlCtClass;
   }
