2788ba8a4508f0546b6321d0523d76b7160b8d8b
BeggarLan <1348270542@qq.com>
2022-07-30 16:56:20 +0800

    Dex2Smali方法

--- aoto-patch-plugin/src/main/java/com/beggar/hotfix/autopatch/JarCommandExecutor.java
+++ aoto-patch-plugin/src/main/java/com/beggar/hotfix/autopatch/JarCommandExecutor.java
@@ -18,6 +18,13 @@ public class JarCommandExecutor {
   // dx工具包, 此处用来把jar包转dex包
   private String mDxToolFailPath;
 
+  // 将dex文件转换成便于阅读的smali文件：java -jar baksmali.jar classes.dex -o outDir
+  // outDir是输出文件夹
+  private String mBaksmaliToolFilePath;
+
+  // 将smali文件重新生成回dex文件：java -jar smali.jar outDir -o classes.dex
+  private String mSmaliToolFilePath;
+
   /**
    * 把jar文件转为dex文件
    *
@@ -32,10 +39,33 @@ public class JarCommandExecutor {
         .append(" --output=" + outputDexFileWithSuffixName)
         .append(" " + jarFileName);
 
-    Process process = Runtime.getRuntime()
-        .exec(commandBuilder.toString(), null, new File(mSubProcessWorkingDirPath));
-    return process;
+    return executeCommand(commandBuilder.toString());
   }
 
+  /**
+   * dex文件转smali文件
+   *
+   * @param dexFileName      输入的dex文件
+   * @param outputSubFileDir 输出文件所在当前目录的子文件夹
+   */
+  public Process dex2Smali(@NonNull String dexFileName, @NonNull String outputSubFileDir)
+      throws IOException {
+    StringBuilder commandBuilder = new StringBuilder();
+    commandBuilder
+        .append("java -jar " + mBaksmaliToolFilePath)
+        .append(" " + dexFileName)
+        .append(" -o " + outputSubFileDir);
+
+    return executeCommand(commandBuilder.toString());
+  }
+
+  /**
+   * 执行命令
+   */
+  @NonNull
+  private Process executeCommand(@NonNull String command) throws IOException {
+    return Runtime.getRuntime().exec(
+        command, null, new File(mSubProcessWorkingDirPath));
+  }
 
 }
